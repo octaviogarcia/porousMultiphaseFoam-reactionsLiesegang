36634734e5128f6202dfa8b13f3af2796e0122ca
david_h.94@hotmail.com <david_h.94@hotmail.com>
2020-05-17 06:27:12 -0300

    Fix menor commit anterior. LEER lo del anterior...Con este hay un bug que todavia no entiendo de donde viene pero es menor, no afectaria los resultados en teoria

--- libraries/reactionModels/secondOrder2/secondOrderv2.C
+++ libraries/reactionModels/secondOrder2/secondOrderv2.C
@@ -397,10 +397,10 @@ void Foam::reactionModels::secondOrderv2::calcCellSizes(){
     const auto& Cfr = mesh.Cf();
 
     auto totalsize = cellsSizes.size();
-    auto rowAmount = 2;                        //number of rows can be parametrized or obtained from somewhere probably but i don't know how
+    auto rowAmount = 2;                         //number of rows can be parametrized or obtained from somewhere probably but i don't know how
     auto rowSize = totalsize/rowAmount;     
 
-    forAll(cellsSizes,i){                   // if there isn't Y expansion neither assymetrical cells can be changed to loop only over first row
+    forAll(cellsSizes,i){                       //if there isn't Y expansion neither assymetrical cells can be changed to loop only over first row
         if(i<rowSize){
             if(i==0){
                 cellsSizes[i].x() = 2 * Cr[i].x();
@@ -413,9 +413,9 @@ void Foam::reactionModels::secondOrderv2::calcCellSizes(){
             }else{
                 cellsSizes[i].x() = 2 * (Cr[i].x() - Cfr[2*(i-1)].x());         //2*i can be generalized to something probably, but i believe it 
                                                                                 // will work the same with more than 2 rows, not sure on asymmetrical cells
-                cellsSizes[i].y() = 2 * (Cr[i].y() - Cfr[i-rowSize].y());       //only valid fot rows of equal lenght (symmetrical)
+                cellsSizes[i].y() = 2 * (Cfr[2*i+1].y() - Cr[i].y());       //only valid for rows of equal lenght (symmetrical) (AND ONLY VALID FOR FIRST ROW & >1 ROWS)
             }
-            for(int row=1; row<=rowAmount-1; row++){
+            for(int row=1; row<rowAmount; row++){
                 cellsSizes[i+rowSize*row] = cellsSizes[i];                      //because there isn't Y expansion.
             }
         }
